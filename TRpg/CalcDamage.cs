513d3a6e9101093af072b74297dfa81bdb6c77b6
tmdwp <tmdwp1010@gmail.com>
2025-02-07 10:31:42 +0900

    [ feat ] 데미지 계산 구현
    오버로딩을 이용
    이후 Interface 추가 시 통합

--- TRpg/CalcDamage.cs
+++ TRpg/CalcDamage.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace TRpg
 {
-
     public class CalcDamage
     {
-        int damage;
-        public int CalcDmg(Player attacker,  Monster defender)
+        int damage; //입힌 피해량
+        int margin; //공격력의 오차 범위 10%
+        public int CalcDmg(Player attacker, Monster defender)
         {
-            damage  = new Random().Next(attacker.Attack - 1, attacker.Attack + 2);
-            defender.Health = defender.Health - damage;
 
+            margin = attacker.Attack / 10;
+            damage = new Random().Next(attacker.Attack - margin, attacker.Attack + margin + 1);
+            
+            defender.Health = defender.Health - damage;
+            
             return damage;
         }
 
         public int CalcDmg(Monster attacker, Player defender)
         {
-            damage = new Random().Next(attacker.Attack - 1, attacker.Attack + 2);
+            margin = attacker.Attack / 10;
+            damage = new Random().Next(attacker.Attack - margin, attacker.Attack + margin + 1);
+
             defender.Health = defender.Health - damage;
 
             return damage;
