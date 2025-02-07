5b386ee0588b4a6708a4cedf2381ba67ea8a424c
tmdwp <tmdwp1010@gmail.com>
2025-02-07 10:03:00 +0900

    [feat] 데미지 계산

---
+++ TRpg/CalcDamage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRpg
+{
+
+    public class CalcDamage
+    {
+        int damage;
+        public int CalcDmg(Player attacker,  Monster defender)
+        {
+            damage  = new Random().Next(attacker.Attack - 1, attacker.Attack + 2);
+            defender.Health = defender.Health - damage;
+
+            return damage;
+        }
+
+        public int CalcDmg(Monster attacker, Player defender)
+        {
+            damage = new Random().Next(attacker.Attack - 1, attacker.Attack + 2);
+            defender.Health = defender.Health - damage;
+
+            return damage;
+        }
+    }
+}
---
+++ TRpg/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+
+public class Player
+{
+    public int Lv { get; set; }
+    public string Name { get; set; }
+    public string Job { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Health { get; set; }
+    public int Gold { get; set; }
+
+    public void PlayerStatus()
+    {
+        Console.WriteLine($"레벨: {Lv}");
+        Console.WriteLine($"이름: {Name}");
+        Console.WriteLine($"직업: {Job}");
+        Console.WriteLine($"공격력: {Attack}");
+        Console.WriteLine($"방어력: {Defense}");
+        Console.WriteLine($"체력: {Health}");
+        Console.WriteLine($"골드: {Gold}G");
+    }
+}
+public class Monster
+    {
+        public int Lv { get; set; }
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public void MonsterStatus()
+        {
+            Console.WriteLine($"레벨: {Lv}");
+            Console.WriteLine($"이름: {Name}");
+            Console.WriteLine($"체력: {Health}");
+            Console.WriteLine($"공격력: {Attack}");
+        }
+    }
+
+
