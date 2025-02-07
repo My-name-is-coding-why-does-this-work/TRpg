bbfd37d22fc2c3c268859f41aaba3956bee59535
tmdwp <tmdwp1010@gmail.com>
2025-02-07 14:45:19 +0900

    Revert "Merge branch 'Kang' into JSJ_CalcDamage"
    This reverts commit 91a688e38708f3e346ccd13c823b2f60126cecc9, reversing
    changes made to 513d3a6e9101093af072b74297dfa81bdb6c77b6.

--- TRpg/Player.cs
+++ TRpg/Player.cs
@@ -3,27 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
-
-public interface ICharacter
+public class Player
 {
-    public bool IsDead { get; set; }
     public int Lv { get; set; }
     public string Name { get; set; }
-    public string IClass { get; set; }
-    public int Attack { get; set; }
-    public int Defense { get; set; }
-    public int Health { get; set; }
-    public int Gold { get; set; }
-    //public void Status();
-}
-
-
-public class Player : ICharacter
-{
-    public bool IsDead { get; set; }
-    public int Lv { get; set; }
-    public string Name { get; set; }
-    public string IClass { get; set; }
+    public string Job { get; set; }
     public int Attack { get; set; }
     public int Defense { get; set; }
     public int Health { get; set; }
@@ -33,33 +17,26 @@ public class Player : ICharacter
     {
         Console.WriteLine($"레벨: {Lv}");
         Console.WriteLine($"이름: {Name}");
-
-        Console.WriteLine($"직업: {IClass}");
+        Console.WriteLine($"직업: {Job}");
         Console.WriteLine($"공격력: {Attack}");
         Console.WriteLine($"방어력: {Defense}");
         Console.WriteLine($"체력: {Health}");
         Console.WriteLine($"골드: {Gold}G");
     }
 }
-
-
-public class Monster : ICharacter
-{
-    public bool IsDead { get; set; }
-    public int Lv { get; set; }
-    public string Name { get; set; }
-    public string IClass { get; set; }
-    public int Attack { get; set; }
-    public int Defense { get; set; }
-    public int Health { get; set; }
-    public int Gold { get; set; }
-
-    public void MonsterStatus()
+public class Monster
     {
-        Console.WriteLine($"레벨: {Lv}");
-        Console.WriteLine($"이름: {Name}");
-        Console.WriteLine($"체력: {Health}");
-        Console.WriteLine($"공격력: {Attack}");
+        public int Lv { get; set; }
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public void MonsterStatus()
+        {
+            Console.WriteLine($"레벨: {Lv}");
+            Console.WriteLine($"이름: {Name}");
+            Console.WriteLine($"체력: {Health}");
+            Console.WriteLine($"공격력: {Attack}");
+        }
     }
-}
+
 
