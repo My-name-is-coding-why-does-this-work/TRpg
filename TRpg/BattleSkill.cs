b981071f4b6317b20ba4fb8132ae139a276604fa
DDanPat <hyoung6855@gmail.com>
2025-02-10 16:57:23 +0900

    [Feat] BattleSkill.cs
    다중공격 기능 추가 및 다중공격이 몬스터에게 데미지 들어가는 기능 추가

--- TRpg/BattleSkill.cs
+++ TRpg/BattleSkill.cs
@@ -44,56 +44,82 @@ namespace TRpg
                     Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.");
                     Console.Write(">>");
                 }
-                //스킬을 사용할 몬스터 선택
-                while (playerturn)
+
+                if (player.SkillList[selectSkill-1].Multiple == true)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Battle!!\n");
-
-                    for (int i = 0; i < BattleList.Count; i++)
+                    for (int i = 0; i<BattleList.Count; i++)
                     {
-                        Console.Write($"-{i + 1} ");
-                        Console.WriteLine($"{BattleList[i].Name} {BattleList[i].Lv} {BattleList[i].Health}");
-                    }
+                        if (!BattleList[i].IsDead) // 수정 : 선택한 몬스터가 사망하지 않았을때만 실행
+                        {
+                            Console.WriteLine($"{BattleList[i].Name} 공격");
+                            Console.ReadKey(true);
 
-                    Console.WriteLine("");
-                    Console.WriteLine("");
-
-                    Console.WriteLine("[내정보]");
-                    Console.WriteLine($"Lv.{player.Lv} {player.Name} ({player.IClass})");
-                    Console.WriteLine($"HP {player.Health}");
-
-
-                    Console.WriteLine("\n[행동]\n");
-                    Console.WriteLine("\n공격할 몬스터를 선택해주세요.");
-                    Console.Write(">>");
-                    int input;
-                    while (!int.TryParse(Console.ReadLine(), out input))
+                            //전달해야 할 선택한 스킬 selectSkill
+                            int dmg = CalcDamage.CalcDmg(player, BattleList[i]/*, selectSkill*/);
+                            if (dmg != 0)
+                                Console.WriteLine($"{BattleList[i].Name}이(가) {dmg} 데미지를 받았습니다.");
+                            else
+                            {
+                                killCount++;
+                                Console.WriteLine($"{BattleList[i].Name}의 체력이 0이 되었습니다.");
+                            }
+                            playerturn = false;
+                        }
+                    } 
+                }
+                else
+                {
+                    //스킬을 사용할 몬스터 선택
+                    while (playerturn)
                     {
-                        Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.");
+                        Console.Clear();
+                        Console.WriteLine("Battle!!\n");
+
+                        for (int i = 0; i < BattleList.Count; i++)
+                        {
+                            Console.Write($"-{i + 1} ");
+                            Console.WriteLine($"{BattleList[i].Name} {BattleList[i].Lv} {BattleList[i].Health}");
+                        }
+
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+
+                        Console.WriteLine("[내정보]");
+                        Console.WriteLine($"Lv.{player.Lv} {player.Name} ({player.IClass})");
+                        Console.WriteLine($"HP {player.Health}");
+
+
+                        Console.WriteLine("\n[행동]\n");
+                        Console.WriteLine("\n공격할 몬스터를 선택해주세요.");
                         Console.Write(">>");
-                    }
+                        int input;
+                        while (!int.TryParse(Console.ReadLine(), out input))
+                        {
+                            Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.");
+                            Console.Write(">>");
+                        }
 
-                    if (input > 0 && input <= BattleList.Count && !BattleList[input - 1].IsDead) // 수정 : 선택한 몬스터가 사망하지 않았을때만 실행
-                    {
-                        Console.WriteLine($"{BattleList[input - 1].Name} 공격");
-                        Console.ReadKey(true);
+                        if (input > 0 && input <= BattleList.Count && !BattleList[input - 1].IsDead) // 수정 : 선택한 몬스터가 사망하지 않았을때만 실행
+                        {
+                            Console.WriteLine($"{BattleList[input - 1].Name} 공격");
+                            Console.ReadKey(true);
 
-                        //전달해야 할 선택한 스킬 selectSkill
-                        int dmg = CalcDamage.CalcDmg(player, BattleList[input - 1]/*, selectSkill*/);
-                        if (dmg != 0)
-                            Console.WriteLine($"{BattleList[input - 1].Name}이(가) {dmg} 데미지를 받았습니다.");
+                            //전달해야 할 선택한 스킬 selectSkill
+                            int dmg = CalcDamage.CalcDmg(player, BattleList[input - 1]/*, selectSkill*/);
+                            if (dmg != 0)
+                                Console.WriteLine($"{BattleList[input - 1].Name}이(가) {dmg} 데미지를 받았습니다.");
+                            else
+                            {
+                                killCount++;
+                                Console.WriteLine($"{BattleList[input - 1].Name}의 체력이 0이 되었습니다.");
+                            }
+                            playerturn = false;
+                        }
                         else
                         {
-                            killCount++;
-                            Console.WriteLine($"{BattleList[input - 1].Name}의 체력이 0이 되었습니다.");
+                            Console.WriteLine("잘못된 입력입니다. 계속 하시려면 아무키를 입력하세요.");
+                            Console.ReadKey(true);
                         }
-                        playerturn = false;
-                    }
-                    else 
-                    {
-                        Console.WriteLine("잘못된 입력입니다. 계속 하시려면 아무키를 입력하세요.");
-                        Console.ReadKey(true);
                     }
                 }
             }
